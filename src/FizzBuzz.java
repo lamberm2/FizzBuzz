308c359d018ad31fed409df34a9efb89566f41df
Alexander Lambermon <alambo@Alexanders-MacBook-Air-2.local>
2023-09-11 18:19:47 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,8 +4,13 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
+        int i = 1;
 
-        for (int i = 1; i < 100; i++) {
+        gerbaflurb(i);
+    }
+
+    private static void gerbaflurb(int i) {
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,6 +34,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i += 1;
         }
     }
 }
