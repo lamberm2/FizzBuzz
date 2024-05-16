8b5e8d996545aa101c76951a4cfac5cc0febad5f
alambo <alexander.lambermon@mail.utoronto.ca>
2024-05-16 19:02:45 -0400

    testing commit right now

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,29 +12,34 @@ class FizzBuzz {
     public static void gerbaflurb(int i) {
         while (i < 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-            i += 1;
+            i = dofizzbuzz(i);
         }
+    }
+
+    private static int dofizzbuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i += 1;
+        return i;
     }
 }
